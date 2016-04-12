8e19e161dfcb34209c830318c58e978c8cc12523
Justin Thomas <jthomas0209@gmail.com>
2016-04-12 16:44:35 -0400

    old app.js removed

--- app/assets/javascripts/app.js
+++
@@ -1,9 +0,0 @@
-angular
-  .module('eatOut',['ui.router','Devise','templates', 'ngResource'])
-  .config(function($stateProvider,$urlRouterProvider){
-    $stateProvider
-      .state('home',{
-        url: '/',
-        templateUrl:'app/views/home.html'
-      });
-  });
