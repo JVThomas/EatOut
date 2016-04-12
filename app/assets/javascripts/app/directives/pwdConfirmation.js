70046394bc9649b7097c9f31905dd51609308139
Justin Thomas <jthomas0209@gmail.com>
2016-04-12 14:00:58 -0400

    Fixed incorrect inequality on pwdConfirmation.

--- app/assets/javascripts/app/directives/pwdConfirmation.js
+++ app/assets/javascripts/app/directives/pwdConfirmation.js
@@ -1,14 +1,16 @@
 function pwdConfirmation(){
-  return{
+  return {
     require:"ngModel",
     scope:{
       password: "=pwdConfirmation"
     },
     link:function(scope, elem, attrs, ngModel){
       ngModel.$validators.pwdConfirmation = function(modelValue){
-        return modelValue = scope.password;
+        debugger;
+        return modelValue === scope.password;
       };
       scope.$watch("password", function(){
+        debugger;
         ngModel.$validate();
       });
     }
